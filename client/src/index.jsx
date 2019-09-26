ff69037eb3d6c5e0271d3dec88640a8360b5a300
vladigolnikov <vladigolnikov@gmail.com>
2019-09-26 00:22:53 -0400

    fix products filepath

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Axios from 'axios';
-import Products from './components/products';
+import Products from './components/products.jsx';
 
 class App extends React.Component {
   constructor(props) {
